d9c7f4fd10428792982a4b790a83440371684e38
bshap27 <rshapiro3@gmail.com>
2017-10-03 13:43:03 -0400

    add user, pet models

--- intakeform/models.py
+++ intakeform/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    created_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=200)
+    primary_breed = models.CharField(max_length=200)
+    mix = models.BooleanField()
+    age = models.IntegerField(default=0)
+    created_date = models.DateTimeField()
+    user_id = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
