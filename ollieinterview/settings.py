33b5538f2874a7d0f3470ad77379ae1393fc6602
bshap27 <rshapiro3@gmail.com>
2017-10-07 10:43:13 -0400

    revert db changes

--- ollieinterview/settings.py
+++ ollieinterview/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ollieinterview.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'local': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'olliedbtest',
         'USER': 'becca',
@@ -85,10 +85,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
