8c101e9df0ceacb32806f9b63622f06e9c2c4f67
bshap27 <rshapiro3@gmail.com>
2017-10-07 10:39:58 -0400

    reconfigure db for heroku

--- ollieinterview/settings.py
+++ ollieinterview/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ollieinterview.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'local': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'olliedbtest',
         'USER': 'becca',
@@ -85,6 +85,10 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
